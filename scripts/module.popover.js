ef62df5f94188eaba0671b0a2f9be52b76ba6829
Warren Shea <warren.shea@gmail.com>
2022-11-06 13:07:06 -0500

    Restructure some code....not sure if I want to keep changes

--- scripts/module.popover.js
+++ scripts/module.popover.js
@@ -121,8 +121,8 @@ storm_eagle.module('popover', function () {
         popover.style.top = "initial";
         popover.style.left = "0px";
       } else if (storm_eagle.client.viewport.is_lg_up()) {
-        popover.style.top = popover_trigger.offsetTop + "px";
-        popover.style.left = popover_trigger.offsetLeft + 40 + "px";
+        popover.style.top = "".concat(popover_trigger.offsetTop, "px");
+        popover.style.left = "".concat(popover_trigger.offsetLeft + 40, "px");
       }
     }
   };
