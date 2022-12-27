8e3c039889d0e88e5af936d7d07d67216f6c9e65
Warren Shea <warren.shea@gmail.com>
2022-12-27 11:43:01 -0500

    Homepage changes/improvements/additions

--- scripts/home.js
+++ scripts/home.js
@@ -3,16 +3,17 @@
 storm_eagle.module('homepage', function () {
   return {
     initialize: function initialize() {
-      document.querySelectorAll(".opacity\\:0\\%").forEach(function (el, index) {
+      document.querySelectorAll("li.opacity\\:0\\%").forEach(function (el, index) {
         setTimeout(function () {
           el.classList.remove("opacity:0%");
           el.classList.add("opacity:100%");
         }, index * 50);
       });
-      document.querySelectorAll(".heading,.description-line,.description-notes-line").forEach(function (el, index) {
+      document.querySelectorAll("h2,.heading,.description-line,.description-notes-line").forEach(function (el, index) {
         setTimeout(function () {
+          el.classList.remove("opacity:0%");
           el.classList.add("opacity:100%");
-        }, index * 50);
+        }, index * 100);
       });
     }
   };
