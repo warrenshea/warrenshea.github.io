4d96f118f62aa907be2e701f232002b1f3132887
Warren Shea <warren.shea@gmail.com>
2023-09-18 16:25:49 -0400

    Initial Commit

---
+++ scripts/archived.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * Disables the telephone number link from calling in medium and up
+ */
+storm_eagle.module('disable_tel_medium_up', function () {
+  return {
+    initialize: function initialize() {
+      document.querySelectorAll('a[href^="tel"]').forEach(function (el) {
+        el.addEventListener("click", function (event) {
+          if (storm_eagle.client.viewport.is_md_up()) {
+            event.preventDefault();
+          }
+        });
+      });
+    }
+  };
+});
