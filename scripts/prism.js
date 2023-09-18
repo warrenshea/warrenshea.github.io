17f2d17b709a85364e0d8960d8ad740c54df8965
Warren Shea <warren.shea@gmail.com>
2023-09-18 12:24:35 -0400

    Minor refactor

--- scripts/prism.js
+++ scripts/prism.js
@@ -4,6 +4,17 @@ storm_eagle.module('prism', function () {
   'use strict';
 
   var self;
+
+  function html_encode(str) {
+    var buf = [];
+
+    for (var i = str.length - 1; i >= 0; i--) {
+      buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+    }
+
+    return buf.join('');
+  }
+
   return {
     initialize: function initialize() {
       self = storm_eagle["prism"];
@@ -13,9 +24,8 @@ storm_eagle.module('prism', function () {
       }
     },
     link_code_with_prism_snippet: function link_code_with_prism_snippet() {
-      document.querySelectorAll("[data-code-reference]").forEach(function (el) {
-        var $cleanse = el.cloneNode(true);
-        document.querySelector("code[data-code-snippet=".concat(el.getAttribute("data-code-reference"))).innerHTML = $cleanse.innerHTML;
+      document.querySelectorAll("[data-code-reference]").forEach(function (el, index) {
+        document.querySelector("code[data-code-snippet=".concat(el.getAttribute("data-code-reference"))).innerHTML = html_encode(el.value);
       });
     },
     force_prism_reinit: function force_prism_reinit() {
