6b7161002c57739df4190cdf9b25faedea117c59
Warren Shea <warren.shea@gmail.com>
2023-09-29 23:33:03 -0400

    Fix code

--- scripts/module.responsive-emulation.js
+++ scripts/module.responsive-emulation.js
@@ -71,25 +71,25 @@ storm_eagle.module('responsive_emulation', function () {
       }
     },
     is_sm_only: function is_sm_only(responsive_emulation_id) {
-      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["sm_min"] && responsive_emulation_state[responsive_emulation_id]["width"] <= breakpoints["sm_max"] ? true : false;
+      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["sm_min"] && responsive_emulation_state[responsive_emulation_id]["width"] < breakpoints["md_min"] ? true : false;
     },
     is_sm_up: function is_sm_up(responsive_emulation_id) {
       return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["sm_min"] ? true : false;
     },
     is_md_down: function is_md_down(responsive_emulation_id) {
-      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["sm_min"] && responsive_emulation_state[responsive_emulation_id]["width"] <= breakpoints["md_max"] ? true : false;
+      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["sm_min"] && responsive_emulation_state[responsive_emulation_id]["width"] < breakpoints["lg_min"] ? true : false;
     },
     is_md_only: function is_md_only(responsive_emulation_id) {
-      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["md_min"] && responsive_emulation_state[responsive_emulation_id]["width"] <= breakpoints["md_max"] ? true : false;
+      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["md_min"] && responsive_emulation_state[responsive_emulation_id]["width"] < breakpoints["lg_min"] ? true : false;
     },
     is_md_up: function is_md_up(responsive_emulation_id) {
       return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["md_min"] ? true : false;
     },
     is_lg_down: function is_lg_down(responsive_emulation_id) {
-      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["sm_min"] && responsive_emulation_state[responsive_emulation_id]["width"] <= breakpoints["lg_max"] ? true : false;
+      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["sm_min"] && responsive_emulation_state[responsive_emulation_id]["width"] < breakpoints["xl_min"] ? true : false;
     },
     is_lg_only: function is_lg_only(responsive_emulation_id) {
-      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["lg_min"] && responsive_emulation_state[responsive_emulation_id]["width"] <= breakpoints["lg_max"] ? true : false;
+      return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["lg_min"] && responsive_emulation_state[responsive_emulation_id]["width"] < breakpoints["xl_min"] ? true : false;
     },
     is_lg_up: function is_lg_up(responsive_emulation_id) {
       return responsive_emulation_state[responsive_emulation_id]["width"] >= breakpoints["lg_min"] ? true : false;
