212b3827be92d44450a80ce8dd8637fd8a4eb275
Warren Shea <warren.shea@gmail.com>
2023-10-12 08:27:18 -0400

    JSPrettier

--- scripts/module.modal.js
+++ scripts/module.modal.js
@@ -1,4 +1,8 @@
-"use strict";
+'use strict';
+/**
+ * data-module=modal         the modal
+ * data-module=modal.trigger the button that opens the modal
+ */
 
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
@@ -13,8 +17,6 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToAr
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 storm_eagle.module('modal', function () {
-  'use strict';
-
   var self;
   var focus_placeholder;
   var modal_first_tab_stop;
@@ -23,7 +25,7 @@ storm_eagle.module('modal', function () {
 
   function modal_close(event) {
     //console.log(event.target.classList);
-    if (event.target.classList.contains("modal-container") || event.target.classList.contains("modal-overlay")) {
+    if (event.target.classList.contains('modal-container') || event.target.classList.contains('modal-overlay')) {
       storm_eagle.modal.close();
     }
   }
@@ -50,12 +52,12 @@ storm_eagle.module('modal', function () {
 
   return {
     initialize: function initialize() {
-      self = storm_eagle["modal"];
+      self = storm_eagle['modal'];
       document.querySelectorAll("[data-module='modal']").forEach(function (el) {
-        var modal_id = el.getAttribute("id");
+        var modal_id = el.getAttribute('id');
         modal_state[modal_id] = {
-          "focusable_elements": [],
-          "remove_focusable_elements": []
+          focusable_elements: [],
+          remove_focusable_elements: []
         };
         self.get_modal_focusable_elements(modal_id);
       });
@@ -64,32 +66,32 @@ storm_eagle.module('modal', function () {
       document.addEventListener('mousedown', modal_close);
       /* updates modal visuals */
 
-      document.querySelector("body").classList.add("overflow:hidden");
+      document.querySelector('body').classList.add('overflow:hidden');
 
       if (modal_trigger) {
         modal_trigger.classList.add('active');
       }
 
       document.getElementById(modal_id).classList.add('active');
-      document.getElementById(modal_id).querySelector(".modal-container").classList.add('active');
+      document.getElementById(modal_id).querySelector('.modal-container').classList.add('active');
       /* removes focus from elements except in modal */
 
-      modal_state[modal_id]["focusable_elements"].forEach(function (el) {
-        el.setAttribute("tabindex", "0");
+      modal_state[modal_id]['focusable_elements'].forEach(function (el) {
+        el.setAttribute('tabindex', '0');
       });
-      modal_state[modal_id]["remove_focusable_elements"].forEach(function (el) {
-        el.setAttribute("tabindex", "-1");
+      modal_state[modal_id]['remove_focusable_elements'].forEach(function (el) {
+        el.setAttribute('tabindex', '-1');
       });
       /* remove focusable elements from nodelist, e.g. popover inside modal */
 
-      modal_state[modal_id]["focusable_elements"] = _toConsumableArray(modal_state[modal_id]["focusable_elements"]).filter(function (el) {
-        return !_toConsumableArray(modal_state[modal_id]["remove_focusable_elements"]).includes(el);
+      modal_state[modal_id]['focusable_elements'] = _toConsumableArray(modal_state[modal_id]['focusable_elements']).filter(function (el) {
+        return !_toConsumableArray(modal_state[modal_id]['remove_focusable_elements']).includes(el);
       });
       /* saves item that opened modal for later */
 
       focus_placeholder = document.activeElement;
-      modal_first_tab_stop = modal_state[modal_id]["focusable_elements"][0];
-      modal_last_tab_stop = modal_state[modal_id]["focusable_elements"][modal_state[modal_id]["focusable_elements"].length - 1];
+      modal_first_tab_stop = modal_state[modal_id]['focusable_elements'][0];
+      modal_last_tab_stop = modal_state[modal_id]['focusable_elements'][modal_state[modal_id]['focusable_elements'].length - 1];
       /* set focus to modal (but not the modal_first_tab_stop */
 
       setTimeout(function () {
@@ -103,7 +105,7 @@ storm_eagle.module('modal', function () {
     },
     close: function close() {
       document.removeEventListener('mousedown', modal_close);
-      document.querySelector("body").classList.remove("overflow:hidden");
+      document.querySelector('body').classList.remove('overflow:hidden');
       /* updates modal visuals */
 
       document.querySelector("[data-module='modal'].active").setAttribute('tabIndex', '-1');
@@ -113,30 +115,30 @@ storm_eagle.module('modal', function () {
         document.querySelector("[data-module='modal.trigger'].active").classList.remove('active');
       }
 
-      document.querySelector("[data-module='modal'].active").querySelector(".modal-container").classList.remove('active');
+      document.querySelector("[data-module='modal'].active").querySelector('.modal-container').classList.remove('active');
       document.querySelector("[data-module='modal'].active").classList.remove('active');
       document.querySelectorAll("[data-module='modal']").forEach(function (modal, index) {
-        var modal_id = modal.getAttribute("id");
+        var modal_id = modal.getAttribute('id');
         /* remove focus from modal elements */
 
-        modal_state[modal_id]["focusable_elements"].forEach(function (el) {
-          el.setAttribute("tabindex", "-1");
+        modal_state[modal_id]['focusable_elements'].forEach(function (el) {
+          el.setAttribute('tabindex', '-1');
         });
         /* remove keyboard event listener */
 
         document.getElementById(modal_id).removeEventListener('keydown', keyboard_modal_focus_trap);
       });
       document.querySelectorAll("[data-module='modal.trigger']").forEach(function (modal_trigger) {
-        modal_trigger.setAttribute("aria-expanded", false);
+        modal_trigger.setAttribute('aria-expanded', false);
       });
       /* set focus to focus_placeholder */
 
       focus_placeholder.focus();
     },
     get_modal_focusable_elements: function get_modal_focusable_elements(modal_id) {
-      modal_state[modal_id]["focusable_elements"] = document.getElementById(modal_id).querySelectorAll(focus_trap_selector); //console.log(modal_state[modal_id]["focusable_elements"]);
+      modal_state[modal_id]['focusable_elements'] = document.getElementById(modal_id).querySelectorAll(focus_trap_selector); //console.log(modal_state[modal_id]["focusable_elements"]);
 
-      modal_state[modal_id]["remove_focusable_elements"] = document.getElementById(modal_id).querySelectorAll(remove_focus_selector); //console.log(modal_state[modal_id]["remove_focusable_elements"]);
+      modal_state[modal_id]['remove_focusable_elements'] = document.getElementById(modal_id).querySelectorAll(remove_focus_selector); //console.log(modal_state[modal_id]["remove_focusable_elements"]);
     }
   };
 });
