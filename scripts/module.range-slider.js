df31eb4d838c75049e1c114e110dff0cd4239518
Warren Shea <warren.shea@gmail.com>
2023-10-12 08:27:05 -0400

    JSPrettier + Refactor

--- scripts/module.range-slider.js
+++ scripts/module.range-slider.js
@@ -1,21 +1,20 @@
-"use strict";
+'use strict';
 
 storm_eagle.module('range_slider', function () {
-  "use strict";
-
   var self;
   var slider_state = {};
   return {
     initialize: function initialize() {
-      self = storm_eagle["range_slider"];
-      document.querySelectorAll("[data-module='range-slider.input-container']").forEach(function (el) {
-        var slider_id = el.querySelector("[data-module='range-slider.input-1']").getAttribute("id");
+      self = storm_eagle['range_slider'];
+      document.querySelectorAll('[data-module="range-slider.input-container"]').forEach(function (el) {
+        var slider_id = el.querySelector('[data-module="range-slider.input-1"]').getAttribute('id');
         slider_state[slider_id] = {
-          "slider_2_id": el.querySelector("[data-module='range-slider.input-2']").getAttribute("id"),
-          "label_container": el.querySelector("[data-module='slider.labels']"),
-          "labels": el.querySelectorAll("[data-module='slider.labels'] > * "),
-          "num_labels": el.querySelectorAll("[data-module='slider.labels'] > * ").length,
-          "slider_fill": el.querySelector("[data-module='slider.fill']")
+          input_container: el,
+          slider_2_id: el.querySelector('[data-module="range-slider.input-2"]').getAttribute('id'),
+          label_container: el.querySelector('[data-module="slider.labels"]'),
+          labels: el.querySelectorAll('[data-module="slider.labels"] > * '),
+          num_labels: el.querySelectorAll('[data-module="slider.labels"] > * ').length,
+          slider_fill: el.querySelector('[data-module="slider.fill"]')
         };
         self.slider_listener(slider_id);
         self.resize_listener(slider_id);
@@ -23,46 +22,44 @@ storm_eagle.module('range_slider', function () {
       });
     },
     ready: function ready() {
-      document.querySelectorAll("[data-module='range-slider.input-1']").forEach(function (el) {
-        var slider_id = el.getAttribute("id");
+      document.querySelectorAll('[data-module="range-slider.input-1"]').forEach(function (el) {
+        var slider_id = el.getAttribute('id');
         self.force_resize(slider_id);
       });
     },
     force_resize: function force_resize(slider_id) {
-      var slider_thumb_width = 26;
-      var container_width = document.getElementById(slider_id).offsetWidth;
-      var new_label_width = (container_width - slider_thumb_width) / (slider_state[slider_id]["num_labels"] - 1);
-      container_width = container_width + new_label_width - slider_thumb_width;
-      slider_state[slider_id]["label_container"].style.width = container_width + "px";
-      slider_state[slider_id]["label_container"].style.left = 0 + slider_thumb_width / 2 - new_label_width / 2 + "px";
+      var container_width = slider_state[slider_id]['input_container'].offsetWidth;
+      var full_label_width = container_width * slider_state[slider_id]['num_labels'] / (slider_state[slider_id]['num_labels'] - 1);
+      slider_state[slider_id]['label_container'].style.width = full_label_width + 'px';
+      var transform_left = 50 / slider_state[slider_id]['num_labels'] * -1;
+      slider_state[slider_id]['label_container'].style.transform = "translateX(".concat(transform_left, "%)");
     },
     slider_listener: function slider_listener(slider_id) {
-      document.getElementById(slider_id).addEventListener("input", function (event) {
+      document.getElementById(slider_id).addEventListener('input', function (event) {
         self.update_slider_track(slider_id);
       });
-      document.getElementById(slider_state[slider_id]["slider_2_id"]).addEventListener("input", function (event) {
+      document.getElementById(slider_state[slider_id]['slider_2_id']).addEventListener('input', function (event) {
         self.update_slider_track(slider_id);
       });
     },
     update_slider_track: function update_slider_track(slider_id) {
       var el_1 = document.getElementById(slider_id);
-      var el_2 = document.getElementById(slider_state[slider_id]["slider_2_id"]);
+      var el_2 = document.getElementById(slider_state[slider_id]['slider_2_id']);
 
       if (parseInt(el_1.value) > parseInt(el_2.value)) {
         var temp = el_1;
         el_1 = el_2;
-        el_2 = temp; // console.log(`swap`);
-      } // console.log(`el_1:${el_1.value} el_2:${el_2.value}`);
+        el_2 = temp;
+      }
 
-
-      var left = Math.round((el_1.value - el_1.getAttribute("min")) / (el_1.getAttribute("max") - el_1.getAttribute("min")) * 100);
-      slider_state[slider_id]["slider_fill"].style.left = left + "%";
-      var percentage = Math.round((el_2.value - el_2.getAttribute("min")) / (el_2.getAttribute("max") - el_2.getAttribute("min")) * 100);
-      slider_state[slider_id]["slider_fill"].style.width = percentage - left + "%";
+      var left = (el_1.value - el_1.getAttribute('min')) / (el_1.getAttribute('max') - el_1.getAttribute('min')) * 100;
+      slider_state[slider_id]['slider_fill'].style.left = left + '%';
+      var percentage = (el_2.value - el_2.getAttribute('min')) / (el_2.getAttribute('max') - el_2.getAttribute('min')) * 100;
+      slider_state[slider_id]['slider_fill'].style.width = percentage - left + '%';
     },
     update_all_slider_track: function update_all_slider_track() {
-      document.querySelectorAll("[data-module='range-slider.input-container']").forEach(function (el) {
-        var slider_id = el.querySelector("[data-module='slider.input-1']").getAttribute("id");
+      document.querySelectorAll('[data-module="range-slider.input-container"]').forEach(function (el) {
+        var slider_id = el.querySelector('[data-module="slider.input-1"]').getAttribute('id');
         self.update_slider_track(slider_id);
       });
     },
