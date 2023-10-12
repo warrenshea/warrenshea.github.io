c3d2de39988c48e1eac6e3069da5a2603c1d4019
Warren Shea <warren.shea@gmail.com>
2023-10-12 08:27:27 -0400

    JSPrettier + Refactor

--- scripts/module.slider.js
+++ scripts/module.slider.js
@@ -1,20 +1,19 @@
-"use strict";
+'use strict';
 
 storm_eagle.module('slider', function () {
-  "use strict";
-
   var self;
   var slider_state = {};
   return {
     initialize: function initialize() {
-      self = storm_eagle["slider"];
-      document.querySelectorAll("[data-module='slider.input-container']").forEach(function (el) {
-        var slider_id = el.querySelector("[data-module='slider.input']").getAttribute("id");
+      self = storm_eagle['slider'];
+      document.querySelectorAll('[data-module="slider.input-container"]').forEach(function (el) {
+        var slider_id = el.querySelector("[data-module='slider.input']").getAttribute('id');
         slider_state[slider_id] = {
-          "label_container": el.querySelector("[data-module='slider.labels']"),
-          "labels": el.querySelectorAll("[data-module='slider.labels'] > * "),
-          "num_labels": el.querySelectorAll("[data-module='slider.labels'] > * ").length,
-          "slider_fill": el.querySelector("[data-module='slider.fill']")
+          input_container: el,
+          label_container: el.querySelector('[data-module="slider.labels"]'),
+          labels: el.querySelectorAll('[data-module="slider.labels"] > * '),
+          num_labels: el.querySelectorAll('[data-module="slider.labels"] > * ').length,
+          slider_fill: el.querySelector('[data-module="slider.fill"]')
         };
         self.slider_listener(slider_id);
         self.resize_listener(slider_id);
@@ -23,31 +22,30 @@ storm_eagle.module('slider', function () {
     },
     ready: function ready() {
       document.querySelectorAll("[data-module='slider.input']").forEach(function (el) {
-        var slider_id = el.getAttribute("id");
+        var slider_id = el.getAttribute('id');
         self.force_resize(slider_id);
       });
     },
     force_resize: function force_resize(slider_id) {
-      var slider_thumb_width = 26;
-      var container_width = document.getElementById(slider_id).offsetWidth;
-      var new_label_width = (container_width - slider_thumb_width) / (slider_state[slider_id]["num_labels"] - 1);
-      container_width = container_width + new_label_width - slider_thumb_width;
-      slider_state[slider_id]["label_container"].style.width = container_width + "px";
-      slider_state[slider_id]["label_container"].style.left = 0 + slider_thumb_width / 2 - new_label_width / 2 + "px";
+      var container_width = slider_state[slider_id]['input_container'].offsetWidth;
+      var full_label_width = container_width * slider_state[slider_id]['num_labels'] / (slider_state[slider_id]['num_labels'] - 1);
+      slider_state[slider_id]['label_container'].style.width = full_label_width + 'px';
+      var transform_left = 50 / slider_state[slider_id]['num_labels'] * -1;
+      slider_state[slider_id]['label_container'].style.transform = "translateX(".concat(transform_left, "%)");
     },
     slider_listener: function slider_listener(slider_id) {
-      document.getElementById(slider_id).addEventListener("input", function (event) {
+      document.getElementById(slider_id).addEventListener('input', function (event) {
         self.update_slider_track(slider_id);
       });
     },
     update_slider_track: function update_slider_track(slider_id) {
       var el = document.getElementById(slider_id);
-      var percentage = Math.round((el.value - el.getAttribute("min")) / (el.getAttribute("max") - el.getAttribute("min")) * 100);
-      slider_state[slider_id]["slider_fill"].style.width = percentage + "%";
+      var percentage = Math.round((el.value - el.getAttribute('min')) / (el.getAttribute('max') - el.getAttribute('min')) * 100);
+      slider_state[slider_id]['slider_fill'].style.width = percentage + '%';
     },
     update_all_slider_track: function update_all_slider_track() {
-      document.querySelectorAll("[data-module='slider.input-container']").forEach(function (el) {
-        var slider_id = el.querySelector("[data-module='slider.input']").getAttribute("id");
+      document.querySelectorAll('[data-module="slider.input-container"]').forEach(function (el) {
+        var slider_id = el.querySelector('[data-module="slider.input"]').getAttribute('id');
         self.update_slider_track(slider_id);
       });
     },
