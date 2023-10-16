886b4d132a172889ee1df7318141b9be19ea5fdd
Warren Shea <warren.shea@gmail.com>
2023-10-16 00:04:52 -0400

    Remove babel

--- scripts/archived.js
+++ scripts/archived.js
@@ -2,17 +2,16 @@
 /**
  * Disables the telephone number link from calling in medium and up
  */
-
-storm_eagle.module('disable_tel_medium_up', function () {
+storm_eagle.module('disable_tel_medium_up', () => {
   return {
-    initialize: function initialize() {
-      document.querySelectorAll('a[href^="tel"]').forEach(function (el) {
-        el.addEventListener('click', function (event) {
+    initialize: () => {
+      document.querySelectorAll('a[href^="tel"]').forEach((el) => {
+        el.addEventListener('click', (event) => {
           if (storm_eagle.client.viewport.is_md_up()) {
             event.preventDefault();
           }
         });
       });
-    }
+    },
   };
 });
