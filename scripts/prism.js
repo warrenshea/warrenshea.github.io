d9c61f7ca5c6920eecc7d4cbb9b60bc20db1020e
Warren Shea <warren.shea@gmail.com>
2023-10-16 00:09:25 -0400

    Remove babel

--- scripts/prism.js
+++ scripts/prism.js
@@ -1,33 +1,29 @@
 'use strict';
-
-storm_eagle.module('prism', function () {
-  var self;
+storm_eagle.module('prism', () => {
+  let self;
 
   function html_encode(str) {
     var buf = [];
-
     for (var i = str.length - 1; i >= 0; i--) {
       buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
     }
-
     return buf.join('');
   }
 
   return {
-    initialize: function initialize() {
+    initialize: () => {
       self = storm_eagle['prism'];
-
       if (document.querySelectorAll('[data-code-reference]').length > 0) {
         self.link_code_with_prism_snippet();
       }
     },
-    link_code_with_prism_snippet: function link_code_with_prism_snippet() {
-      document.querySelectorAll('[data-code-reference]').forEach(function (el, index) {
-        document.querySelector("code[data-code-snippet=".concat(el.getAttribute('data-code-reference'))).innerHTML = html_encode(el.value);
+    link_code_with_prism_snippet: () => {
+      document.querySelectorAll('[data-code-reference]').forEach((el, index) => {
+        document.querySelector(`code[data-code-snippet=${el.getAttribute('data-code-reference')}`).innerHTML = html_encode(el.value);
       });
     },
-    force_prism_reinit: function force_prism_reinit() {
+    force_prism_reinit: () => {
       Prism.highlightAll();
-    }
+    },
   };
 });
