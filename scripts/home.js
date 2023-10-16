3c7977f2e3f251154e448ac32f54576edbfc82d0
Warren Shea <warren.shea@gmail.com>
2023-10-16 00:05:27 -0400

    Remove babel

--- scripts/home.js
+++ scripts/home.js
@@ -1,20 +1,19 @@
 'use strict';
-
-storm_eagle.module('homepage', function () {
+storm_eagle.module('homepage', () => {
   return {
-    initialize: function initialize() {
-      document.querySelectorAll('li.opacity\\:0\\%').forEach(function (el, index) {
-        setTimeout(function () {
+    initialize: () => {
+      document.querySelectorAll('li.opacity\\:0\\%').forEach((el, index) => {
+        setTimeout(() => {
           el.classList.remove('opacity:0%');
           el.classList.add('opacity:100%');
         }, index * 45);
       });
-      document.querySelectorAll('h2,.heading,.description-line,.description-notes-line').forEach(function (el, index) {
-        setTimeout(function () {
+      document.querySelectorAll('h2,.heading,.description-line,.description-notes-line').forEach((el, index) => {
+        setTimeout(() => {
           el.classList.remove('opacity:0%');
           el.classList.add('opacity:100%');
         }, index * 25);
       });
-    }
+    },
   };
 });
