8d7f7c4bd47ddefaf2826be4a4d308d7acac9715
Warren Shea <warren.shea@gmail.com>
2023-11-18 16:36:38 -0500

    Arrow functions

--- scripts/module.color-picker.js
+++ scripts/module.color-picker.js
@@ -1,13 +1,12 @@
 'use strict';
 storm_eagle.module('color_picker', () => {
-  let self;
   let baseColours = ['#3169b2', '#ab1e23', '#f4c026', '#1c3b65', '#27528c', '#789ccc', '#9bb6d9', '#becfe6', '#80171a', '#9b1b20', '#b63b3f', '#c0575a', '#d58f91', '#b7901d', '#fdf0c9', '#000000', '#202020', '#404040', '#808080', '#c0c0c0', '#e0e0e0', '#f0f0f0', '#ffffff', '#d08888', '#a01010', '#500808', '#84b48e', '#09681d', '#05340f', '#c0791d', '#eeeeec'];
   let baseName = ['blue', 'red', 'yellow', 'yale-blue', 'yinmn-blue', 'vista-blue', 'powder-blue', 'columbia-blue', 'falu-red', 'carmine', 'auburn', 'bittersweet-shimmer', 'old-rose', 'dark-goldenrod', 'dutch-white', 'black', 'eerie-black', 'onyx', 'gray', 'silver', 'platinum', 'white-smoke', 'white', 'negative-light', 'negative', 'negative-dark', 'positive-light', 'positive', 'positive-dark', 'warning'];
   /**
    * Function to convert HEX to RGB
    * @param  {string} color to match
    **/
-  function hex2rgb(colour) {
+  const hex2rgb = (colour) => {
     let cr, cg, cb;
     if (colour.charAt(0) === '#') {
       colour = colour.substr(1);
@@ -22,9 +21,6 @@ storm_eagle.module('color_picker', () => {
   }
 
   return {
-    initialize: () => {
-      self = storm_eagle['color_picker'];
-    },
     find_closest_color: () => {
       let color = document.getElementById('color-picker').value;
       let colorRgb = hex2rgb(color);
@@ -34,7 +30,7 @@ storm_eagle.module('color_picker', () => {
       // Create an emtyp array for the difference betwwen the colors
       let differenceArray = [];
       // Function to find the smallest value in an array
-      let smallestValue = function (array) {
+      const smallestValue = (array) => {
         return Math.min.apply(Math, array);
       };
       // Convert the HEX color in the array to RGB colors, split them up to R-G-B, then find out the difference between the "color" and the colors in the array
