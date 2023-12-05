732e3c7b72cd31fad7807a51048e95cd044e3b0a
Warren Shea <warren.shea@gmail.com>
2023-12-04 22:16:31 -0500

    Refactor

--- scripts/module.slider.js
+++ scripts/module.slider.js
@@ -1,59 +1,62 @@
 'use strict';
 storm_eagle.module('slider', () => {
   let self;
-  let slider_state = {};
+  let module_state = {};
+
+  const handle_slider_change = (event) => {
+    self.update_slider_track(event.currentTarget.getAttribute('id'));
+  };
 
   return {
     initialize: () => {
-      self = storm_eagle['slider'];
-      document.querySelectorAll('[data-module="slider.input-container"]').forEach((el) => {
-        let slider_id = el.querySelector("[data-module='slider.input']").getAttribute('id');
-        slider_state[slider_id] = {
-          input_container: el,
-          label_container: el.querySelector('[data-module="slider.labels"]'),
-          labels: el.querySelectorAll('[data-module="slider.labels"] > * '),
-          num_labels: el.querySelectorAll('[data-module="slider.labels"] > * ').length,
-          slider_fill: el.querySelector('[data-module="slider.fill"]'),
+      self = storm_eagle.slider;
+      module_state = {};
+      document.querySelectorAll('[data-module="slider.input-container"]').forEach((container) => {
+        const el = container.querySelector(":scope > [data-module='slider.input']");
+        const id = el.getAttribute('id');
+        module_state[id] = {
+          id,
+          el,
+          slider_container: container,
+          label_container: container.querySelector(':scope > [data-module="slider.labels"]'),
+          labels: container.querySelectorAll(':scope > [data-module="slider.labels"] > * '),
+          slider_fill: container.querySelector(':scope > [data-module="slider.background"] > [data-module="slider.fill"]'),
         };
-        self.slider_listener(slider_id);
-        self.resize_listener(slider_id);
-        self.update_slider_track(slider_id);
+        self.manage_slider_input_listener(id);
+        self.update_slider_track(id);
+        self.resize_listener(id);
       });
     },
     ready: () => {
       document.querySelectorAll("[data-module='slider.input']").forEach((el) => {
-        let slider_id = el.getAttribute('id');
-        self.force_resize(slider_id);
+        self.label_resize(el.getAttribute('id'));
       });
     },
-    force_resize: (slider_id) => {
-      let container_width = slider_state[slider_id]['input_container'].offsetWidth;
-      let full_label_width = (container_width * slider_state[slider_id]['num_labels']) / (slider_state[slider_id]['num_labels'] - 1);
-      slider_state[slider_id]['label_container'].style.width = `${full_label_width}px`;
-      let transform_left = (50 / slider_state[slider_id]['num_labels']) * -1;
-      slider_state[slider_id]['label_container'].style.transform = `translateX(${transform_left}%)`;
+    manage_slider_input_listener: (id) => {
+      const { el } = module_state[id];
+      el.removeEventListener('input', handle_slider_change);
+      el.addEventListener('input', handle_slider_change);
     },
-    slider_listener: (slider_id) => {
-      document.getElementById(slider_id).addEventListener('input', (event) => {
-        self.update_slider_track(slider_id);
-      });
+    update_slider_track: (id) => {
+      const { el, slider_fill } = module_state[id];
+      const percentage = Math.round(((el.value - el.getAttribute('min')) / (el.getAttribute('max') - el.getAttribute('min'))) * 100);
+      slider_fill.style.width = `${percentage}%`;
     },
-    update_slider_track: (slider_id) => {
-      let el = document.getElementById(slider_id);
-      let percentage = Math.round(((el.value - el.getAttribute('min')) / (el.getAttribute('max') - el.getAttribute('min'))) * 100);
-      slider_state[slider_id]['slider_fill'].style.width = `${percentage}%`;
+    label_resize: (id) => {
+      const { slider_container, labels, label_container } = module_state[id];
+      const num_labels = labels.length;
+      const container_width = slider_container.offsetWidth;
+      const full_label_width = (container_width * num_labels) / (num_labels - 1);
+      label_container.style.width = `${full_label_width}px`;
+      const transform_left = (50 / num_labels) * -1;
+      label_container.style.transform = `translateX(${transform_left}%)`;
     },
-    update_all_slider_track: () => {
-      document.querySelectorAll('[data-module="slider.input-container"]').forEach((el) => {
-        let slider_id = el.querySelector('[data-module="slider.input"]').getAttribute('id');
-        self.update_slider_track(slider_id);
-      });
-    },
-    resize_listener: (slider_id) => {
-      const force_resize = () => {
-        return self.force_resize(slider_id);
-      }
-      storm_eagle.resize_observer(document.getElementById(slider_id), force_resize);
+    resize_listener: (id) => {
+      const { el } = module_state[id];
+      const label_resize = () => {
+        self.label_resize(id);
+      };
+      storm_eagle.resize_observer(el, label_resize);
     },
   };
 });
