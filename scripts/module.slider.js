178d56ed20f2382dc2a9d7b6a2118514c01e9917
Warren Shea <warren.shea@gmail.com>
2023-12-27 08:19:39 -0500

    Minor variable rename

--- scripts/module.slider.js
+++ scripts/module.slider.js
@@ -1,17 +1,15 @@
 'use strict';
 storm_eagle.module('slider', () => {
   let self;
-  let module_state = {};
-
+  let state = {};
   return {
     initialize: () => {
       self = storm_eagle.slider;
-      module_state = {};
+      state = {};
       document.querySelectorAll('[data-module="slider.input-container"]').forEach((container) => {
         const el = container.querySelector(":scope > [data-module='slider.input']");
         const id = el.getAttribute('id');
-        module_state[id] = {
-          id,
+        state[id] = {
           el,
           slider_container: container,
           label_container: container.querySelector(':scope > [data-module="slider.labels"]'),
@@ -28,23 +26,23 @@ storm_eagle.module('slider', () => {
         self.ui.update_slider_track(id);
       },
       label_resize: (id) => {
-        const { slider_container, labels, label_container } = module_state[id];
+        const { slider_container, labels, label_container } = state[id];
         const num_labels = labels.length;
         const container_width = slider_container.offsetWidth;
         const full_label_width = (container_width * num_labels) / (num_labels - 1);
-        label_container.style.width = `${full_label_width}px`;
         const transform_left = (50 / num_labels) * -1;
+        label_container.style.width = `${full_label_width}px`;
         label_container.style.transform = `translateX(${transform_left}%)`;
       },
       update_slider_track: (id) => {
-        const { el, slider_fill } = module_state[id];
+        const { el, slider_fill } = state[id];
         const percentage = Math.round(((el.value - el.getAttribute('min')) / (el.getAttribute('max') - el.getAttribute('min'))) * 100);
         slider_fill.style.width = `${percentage}%`;
       },
     },
     event_listeners: {
       initialize: (id) => {
-        const { el } = module_state[id];
+        const { el } = state[id];
         el.removeEventListener('input', self.event_listeners.handle_slider_change);
         el.addEventListener('input', self.event_listeners.handle_slider_change);
         self.event_listeners.resize_listener(id);
@@ -53,7 +51,7 @@ storm_eagle.module('slider', () => {
         self.ui.update_slider_track(event.currentTarget.getAttribute('id'));
       },
       resize_listener: (id) => {
-        const { el } = module_state[id];
+        const { el } = state[id];
         const label_resize = () => {
           self.ui.label_resize(id);
         };
